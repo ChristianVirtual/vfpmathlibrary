2c35d89970b8274c56d6518c193a64dcebb28ba9
grundmann.matthias <grundmann.matthias@8cc4a874-8500-11dd-a143-0dd59a09f4aa>
2009-02-06 22:44:51 +0000

    Added unittests.

---
+++ unittests/Classes/unittestsAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  unittestsAppDelegate.h
+//  unittests
+//
+//  Created by Matthias Grundmann on 2/6/09.
+//  Copyright Matthias Grundmann 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface unittestsAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow* window_;
+	UITextView* text_view_;
+}
+
+@end
+
